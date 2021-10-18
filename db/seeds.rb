134ed5b22c2b90a2df6f629a31526d4528c8e1c6
Lancerabbit <lancerabbit@gmail.com>
2021-10-18 13:31:18 +0800

    setup test data and add setup project flow on README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 %w[language math chemical algorithms programming].each do |category|
   Category.create(name: category)
 end
+
+%w[admin test].each do |user|
+  User.create(
+    email: "#{user}@test.com",
+    password: "#{user}12345",
+    is_admin: user == 'admin'
+  )
+end
